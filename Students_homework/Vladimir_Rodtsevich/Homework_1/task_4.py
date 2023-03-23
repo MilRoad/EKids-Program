e2475a9d91d100036e1027ecdef4bd45d9d89813
Leonid Rodtsevich <lrodtsevich@gmail.com>
2023-03-23 21:28:27 +0300

    HW1

--- Students_homework/Vladimir_Rodtsevich/Homework_1/task_4.py
+++ Students_homework/Vladimir_Rodtsevich/Homework_1/task_4.py
@@ -35,3 +35,15 @@
 2
 Выход 3: 'Недосып'
 """
+print ("надо")
+a= int(input())
+print ("не надо")
+b = int(input())
+print ("поспал")
+c =int(input())
+if c>b:
+    print("пересып")
+elif c<a:
+    print("Недосып")
+else:
+    print ("нормально")
