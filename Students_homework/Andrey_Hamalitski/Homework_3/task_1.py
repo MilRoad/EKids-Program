dd4e81940a7e9faf6c0ae08064e723f9a3ff8db5
LAPTOP-O6J8JK7I\gnimi <roman_g@tut.by>
2023-04-01 10:05:35 +0300

    Homework 2

---
+++ Students_homework/Andrey_Hamalitski/Homework_3/task_1.py
@@ -0,0 +1,22 @@
+"""
+Напишите функцию sum_range(start, end),
+которая суммирует все целые числа от значения «start» до величины «end» включительно, и возвращает эту сумму.
+Если пользователь задаст первое число большее чем второе, просто поменяйте их местами.
+
+Запустите свою программу и проверьте, что у вас не возникает AssertionError.
+(Ключевое слово assert сверяет результат вашей функции с правильным значением)
+"""
+
+
+def sum_range(start, end):
+    pass
+    if start > end:
+        start, end = end, start
+    return sum(range(start, end+1))
+assert sum_range(2, 12) == 77
+assert sum_range(-4, 4) == 0
+assert sum_range(3, 2) == 5
+a, b = map(int, input().split())
+print(sum_range(a, b))
+
+
